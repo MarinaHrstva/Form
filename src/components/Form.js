3a58532565e539c7336e93954ff18c5dc980725f
Marina Hristova <marina.ts.hristova@gmail.com>
2022-09-07 11:13:38 +0300

    complete the skeleton of the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,38 +2,91 @@ const Form = () => {
     return (
         <div className="form-wrapper">
             <form>
-                <label htmlFor="firsName">First Name</label>
+                <div>
+                    <label htmlFor="firsName">First Name</label>
+                    <input
+                        type="text"
+                        name="firstName"
+                        id="firstName"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        id="lastName"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="age">Age</label>
+                    <input
+                        type="number"
+                        name="age"
+                        id="age"
+                        defaultValue={18}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="Female">Female</label>
+                    <input
+                        type="radio"
+                        name="gender"
+                        id="female"
+                    />
+                    <label htmlFor="Female">Male</label>
+                    <input
+                        type="radio"
+                        name="gender"
+                        id="male"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="city">City:</label>
+                    <select
+                        name="city"
+                        id="city"
+                    >
+                        <option value="Sofia">Sofia</option>
+                        <option value="Plovdiv">Plovdiv</option>
+                        <option value="Varna">Varna</option>
+                        <option value="Burgas">Burgas</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="comment">Add comment:</label>
+                    <textarea
+                        name="comment"
+                        id=""
+                        cols="30"
+                        rows="10"
+                    ></textarea>
+                </div>
+
+                <div>
+                    <input
+                        type="checkbox"
+                        name="newsletter"
+                        id="newsletter"
+                    />
+                    <label htmlFor="newsletter">
+                        Sign me up for the newsletter!
+                    </label>
+                </div>
+
                 <input
-                    type="text"
-                    name="firstName"
-                    id="firstName"
+                    type="submit"
+                    value="SUBMIT"
                 />
-                <label htmlFor="lastName">Last Name</label>
-                <input
-                    type="text"
-                    name="lastName"
-                    id="lastName"
-                />
-                <label htmlFor="email">Email</label>
-                <input
-                    type="text"
-                    name="email"
-                    id="email"
-                />
-                <label htmlFor="age">Age</label>
-                <input
-                    type="number"
-                    name="age"
-                    id="age"
-                    defaultValue={18}
-                />
-                <label htmlFor="gender">Gender</label>
-                <input
-                    type="radio"
-                    name="gender"
-                    id="gender"
-                />
-               
             </form>
         </div>
     );
